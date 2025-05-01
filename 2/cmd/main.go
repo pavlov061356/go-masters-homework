6d68ccd9133b37a780b2f7a61f42ff71be9a6f61
Pavlov Vladimir <pavlovvova923@gmail.com>
2025-05-01 08:46:55 +0300

    review update

--- 2/cmd/main.go
+++ 2/cmd/main.go
@@ -4,25 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"pavlov061356/go-masters-homework/2/internal/api"
-	"strconv"
 )
 
 func main() {
-	// Считываем переменную окружения PORT
-	var port int
-	portString := os.Getenv("PORT")
-	port, err := strconv.Atoi(portString)
-	if err != nil {
-		port = 8080
-	}
+	port := 8080
 
-	// Ргеистрируем хендлеры
 	api := &api.API{}
 	http.HandleFunc("/russian-roulette", api.RussianRouletteHandler)
 	http.HandleFunc("/random-err", api.RandomErrHandler)
 
-	// Запускаем сервер
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
