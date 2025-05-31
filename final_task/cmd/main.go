4d8d8f5ebfb52156f9a0be6d99b5f7edcb692680
Pavlov Vladimir <pavlovvova923@gmail.com>
2025-05-31 13:47:52 +0300

    add golang ci setup

---
+++ final_task/cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func init() {
+	log.Logger = zerolog.New(
+		zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			NoColor:    true,
+			TimeFormat: "2006-01-02 15:04:05",
+		},
+	).With().Timestamp().Logger().With().Caller().Logger()
+}
+
+func main() {
+	log.Debug().Msg("Начало работы приложения")
+}
