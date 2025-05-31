ede4798a2b7d03e8ab296e3e6520ba35c2b56fcd
Pavlov Vladimir <pavlovvova923@gmail.com>
2025-05-31 15:50:10 +0300

    add color to logger

--- final_task/cmd/main.go
+++ final_task/cmd/main.go
@@ -11,7 +11,6 @@ func init() {
 	log.Logger = zerolog.New(
 		zerolog.ConsoleWriter{
 			Out:        os.Stdout,
-			NoColor:    true,
 			TimeFormat: "2006-01-02 15:04:05",
 		},
 	).With().Timestamp().Logger().With().Caller().Logger()
